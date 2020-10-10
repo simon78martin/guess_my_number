fa6df5c678dad298cd1bdd83097e1f8ff9741403
Mart1 Simon <martinsimon.ch@gmail.com>
2020-10-10 11:52:07 +0200

    Création de la class machine pour pouvoir réutiliser le code

--- guess_my_number1.py
+++ guess_my_number1.py
@@ -9,21 +9,40 @@ import random
 
 MIN = 0
 MAX = 1000
-
+ 
+class GuessMachine():
+    """
+    I have a nb in nb, guess it
+    + self.number_to_guess is generated during creation of the object
+    + use 'guess(num)' method to make a guess
+    + I'll count the number of attempt in self.number_of_attempt
+    """
+    def __init__(self):
+        self.number_to_guess = random.randint(MIN, MAX)
+        self.number_of_attempts = 0
+        
+    def guess(self, num):
+        self.number_of_attempts +=1
+        if num < self.number_to_guess:
+            return "too low"
+        elif num > self.number_to_guess:
+            return "too high"
+        else: 
+            return "found"
+    
 if __name__ == '__main__':
-  number_to_guess =random.randint(MIN,MAX)
+  guess_machine = GuessMachine()
   print("try to guess a number between %d and %d" % (MIN, MAX))
 
   while True:
     user_input = input("your guess ?")
     try:
       user_attempt = int(user_input)
-      if user_attempt == number_to_guess:
-        print("you could find a number i had in my mind")
+      result = guess_machine.guess(user_attempt)
+      if result == 'found':
+        print("you could find a number i had in my mind in %d attempts" % guess_machine.number_of_attempts)
         break
-      elif user_attempt < number_to_guess:
-        print("too low")
       else :
-        print("too high")
+        print(result)
     except ValueError:
         print("not an integer")
